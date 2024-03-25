7be3930361835d63e55b95aceadb3be722092f03
William Garcia <williamw.garcia99@gmail.com>
2024-03-25 14:00:01 -0400

    made slight modifications to the layout of the welcome page and also hid buttons that should only show when user is authenticated

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,22 +7,22 @@ import { NavLink } from "react-router-dom";
 
 function Welcome() {
   return (
-    <article className="mb-5 flex h-full flex-col gap-5 sm:mb-20 sm:justify-around">
-      <figure className=" flex flex-col items-center gap-10 sm:mx-4 sm:mt-4 sm:flex-row xl:mx-36">
+    <article className="flex h-full flex-col gap-5 sm:mx-10 sm:justify-around ">
+      <figure className=" flex flex-col items-center gap-10 overflow-y-hidden sm:grid sm:grid-cols-2 sm:justify-items-center sm:gap-10 ">
         <img
           src={welcome}
           alt="Volunteers handing out food"
-          className="w-full sm:w-1/2 sm:max-w-3xl  "
+          className="w-full md:max-w-[700px] "
         />
-        <figcaption className=" flex w-80 flex-col items-center gap-5 font-robotoslab sm:flex-grow">
-          <p className="sm:text-2xl lg:text-4xl">
+        <figcaption className="flex items-center gap-5 font-robotoslab   sm:flex-col md:w-3/4">
+          <p className=" text-xl sm:text-2xl md:text-3xl">
             Transforming excess into impact. Join us in reshaping communities
             and ensuring no meal goes to waste.
           </p>
-          <NavLink to="/login">
+          <NavLink to="/login" className={"flex-shrink-0"}>
             <Button
               disabled={false}
-              className=" hover:bg-green-500 hover:font-bold lg:text-xl "
+              className="hover:bg-green-500 hover:font-bold lg:text-xl "
             >
               Get Started
             </Button>
@@ -31,7 +31,7 @@ function Welcome() {
       </figure>
       <section className="flex flex-col items-center gap-10 font-robotoslab sm:flex-row sm:items-baseline sm:justify-evenly ">
         <div className="flex w-64 flex-col items-center text-center  ">
-          <img src={easy} alt="fingers snapping" className="w-14 md:w-1/2" />
+          <img src={easy} alt="fingers snapping" className="w-14 md:w-1/3" />
           <h3 className="text-xl font-bold">Easy Donation Posting</h3>
           <p>Quickly share surplus food and make a meaningful impact!</p>
         </div>
@@ -39,7 +39,7 @@ function Welcome() {
           <img
             src={circleEnvelope}
             alt="image of an evelope"
-            className="w-14 md:w-1/2"
+            className="w-14 md:w-1/3"
           />
           <h3 className="text-xl font-bold">
             Instant Messaging for Coordination
@@ -47,7 +47,7 @@ function Welcome() {
           <p>Coordinate pickup details instantly!</p>
         </div>
         <div className="flex w-64  flex-col items-center text-center ">
-          <img src={delivery} alt="image of box" className="w-14 md:w-1/2" />
+          <img src={delivery} alt="image of box" className="w-14 md:w-1/3" />
           <h3 className="text-xl font-bold">Simple Donation Claiming</h3>
           <p>Get what you need hassle-free!</p>
         </div>
