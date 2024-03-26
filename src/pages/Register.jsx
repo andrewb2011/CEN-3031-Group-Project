0cc6521a73fb5bf523c6672eaa2415744ff560bd
William Garcia <williamw.garcia99@gmail.com>
2024-03-26 16:12:49 -0400

    finished styling of the Registration page using tailwind settings instead of regular CSS

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,11 +2,11 @@ import RegistrationForm from "../features/authentication/components/Registration
 
 function Register() {
   return (
-    <div className="register-container">
-      <div className="left">
-        <div className="image-container"></div>
+    <div className="flex h-full ">
+      <div className="hidden min-[900px]:block min-[900px]:flex-[2_0_auto]   ">
+        <div className='bg-[url("../../public/images/registerPageImg.jpg")] bg-cover w-full h-full xl:bg-center xl:w-[80%] min-[1800px]:w-[60%]'></div>
       </div>
-      <div className="right">
+      <div className="flex items-center justify-center flex-grow xl:justify-start ">
         <RegistrationForm />
         {/* Any other JSX can go here. For example, any other components or info that should display in the Registration page that is outside the RegistrationForm */}
       </div>
