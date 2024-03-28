bf64ada93e5160d24cd996014643f7bcc62b2b18
jon vargas <vargasjonathan646@gmail.com>
2024-03-28 12:21:13 -0400

    These are the changes to the login.jsx/LoginForm.jsx for the styling to be updated, index.css was changed to add the picture for login styling

--- src/features/authentication/components/LoginForm.jsx
+++ src/features/authentication/components/LoginForm.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, Link } from "react-router-dom";
-
 import { signIn } from "../services/authService";
+import Button from "../../../components/ui/Button"; // Adjust path as necessary
+import { InputField } from "../../../components/ui/InputField"; // Adjust path as necessary
+import { twMerge } from "tailwind-merge";
 
-function Login() {
+function Login({ className }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
@@ -15,85 +17,64 @@ function Login() {
   async function handleLogin(event) {
     event.preventDefault();
 
-    //If either of these attributes is blank then don't proceed
     if (!email || !password) return;
     try {
       setIsLoading(true);
-      const userData = {
-        email,
-        password,
-      };
-      const { error } = await signIn(userData);
+      const { error } = await signIn({ email, password });
 
       if (error) {
-        throw new Error(error.error_description || error.message);
+        alert("Failed to log in: " + error.message);
       } else {
-        //if successfully logged in, then ...
         alert("Successfully logged in!");
         navigate("/home");
       }
-    } catch (message) {
-      alert(message);
+    } catch (error) {
+      alert("Error: " + error.message);
     } finally {
       setIsLoading(false);
     }
   }
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        placeholder="Your-email@example.com"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        style={{ outline: "none" }}
-      />
-      <div
-        style={{
-          display: "flex",
-          margin: " 10px 0 20px 0",
-          padding: "15px",
-          alignItems: "center",
-          columnGap: "5px",
-          width: "75%",
-          border: "1px solid #ccc",
-          boxSizing: "border-box",
-          outline: "none",
-        }}
-      >
-        <input
+    <div className={twMerge("w-2/3", className)}>
+      <h1 className="text-4xl text-center font-abrilfatface">Login</h1>
+      <form onSubmit={handleLogin} className="flex flex-col gap-6 mt-4">
+        <InputField
+          labelName="Email Address"
+          type="email"
+          placeholder="Your-email@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <InputField
+          labelName="Password"
           type={isPasswordVisible ? "text" : "password"}
-          style={{
-            width: "100%",
-            border: "none",
-            padding: 0,
-            margin: 0,
-            outline: "none",
-          }}
           placeholder="Password"
-          minLength="8"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-
-        <FontAwesomeIcon
-          style={{ cursor: "pointer" }}
-          icon={isPasswordVisible ? faEye : faEyeSlash}
-          size="xl"
-          onClick={() => setIsPasswordVisible((state) => !state)}
-        />
-      </div>
-      <Link to="/register">Create account</Link>
-      <button
-        style={{ marginBottom: "20px" }}
-        disabled={isLoading}
-        type="submit"
-      >
-        Submit
-      </button>
-    </form>
+        >
+          <FontAwesomeIcon
+            className="absolute right-0 p-1 cursor-pointer"
+            icon={isPasswordVisible ? faEye : faEyeSlash}
+            onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+          />
+        </InputField>
+        <div className="flex justify-center">
+          <Button
+            disabled={isLoading}
+            type="submit"
+            className="px-6 py-2 text-xl font-bold font-robotoslab hover:bg-green-500"
+          >
+            {isLoading ? 'Logging in...' : 'Login'}
+          </Button>
+        </div>
+        <div className="text-center">
+          <Link to="/register" className="font-bold text-blue-500 hover:underline">
+            Don't have an account? Register
+          </Link>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,14 @@ import LoginForm from "../features/authentication/components/LoginForm";
 
 function Login() {
   return (
-    <div>
-      <LoginForm />
-      {/* Any other JSX can go here. For example, any other components or info that should display in the Login page that is outside the LoginForm */}
+    <div className="login-container">
+      <div className="login-left">
+        <div className="login-image-container"></div>
+      </div>
+      <div className="login-right">    
+       <LoginForm />
+        {/* Any other JSX can go here. For example, any other components or info that should display in the Login page that is outside the LoginForm */}
+      </div>
     </div>
   );
 }
