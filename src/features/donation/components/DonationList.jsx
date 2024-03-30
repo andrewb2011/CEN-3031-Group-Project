2449833ddcec19f0a9ae2237ad3a105956885b17
andrewb2011 <andrew.ballard@ufl.edu>
2024-03-30 10:06:14 -0400

    Changed location of date and Org name. and set width of modal window

--- src/features/donation/components/DonationList.jsx
+++ src/features/donation/components/DonationList.jsx
@@ -36,21 +36,21 @@ function DonationList({ user }) {
           <h2 className="mb-2 text-lg font-bold">{selectedPost.title}</h2>
           <div className="p-4 mb-4 bg-[#FAC710] bg-opacity-15 border rounded-lg border-orange">
             <p className="mb-4 text-gray-700">{selectedPost.description}</p>
-          </div>
-          <div className="flex gap-3">
-            <div className="p-2 mb-4 border border-gray-500 rounded-sm">
-              <FontAwesomeIcon icon={faCalendar} className="" />{" "}
-              {new Date(selectedPost.created_at).toLocaleDateString("en-US", {
-                day: "numeric",
-                month: "short",
-              })}
-            </div>
-            <div className="relative flex items-center p-2 mb-4 border border-gray-500 rounded-sm">
-              {selectedPost.profiles.organization_name}
-            </div>
-            <div className="flex items-center p-2 mb-4 border border-gray-500 rounded-sm">
-              <div className="w-3 h-3 mr-2 bg-green-500 rounded-full"></div>
-              <div>Available</div>
+            <div className="flex gap-3">
+              <div className="p-2 mb-4 border border-gray-500 rounded-sm">
+                <FontAwesomeIcon icon={faCalendar} className="" />{" "}
+                {new Date(selectedPost.created_at).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                })}
+              </div>
+              <div className="relative flex items-center p-2 mb-4 border border-gray-500 rounded-sm">
+                {selectedPost.profiles.organization_name}
+              </div>
+              <div className="flex items-center p-2 mb-4 border border-gray-500 rounded-sm">
+                <div className="w-3 h-3 mr-2 bg-green-500 rounded-full"></div>
+                <div>Available</div>
+              </div>
             </div>
           </div>
 
