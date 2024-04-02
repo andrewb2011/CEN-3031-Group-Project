5863d651dffc8592d83ff0265b42877b2272582f
jon vargas <vargasjonathan646@gmail.com>
2024-04-02 12:34:07 -0400

    Added the intended functionality to Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,15 @@ import LoginForm from "../features/authentication/components/LoginForm";
 
 function Login() {
   return (
-    <div>
-      <LoginForm />
-      {/* Any other JSX can go here. For example, any other components or info that should display in the Login page that is outside the LoginForm */}
+    <div className="flex h-full">
+      {/* Change image for larger screens */}
+      <div className="hidden min-[900px]:block min-[900px]:flex-[2_0_auto]">
+        <div className='bg-[url("../../src/assets/pexels-pixabay-262967.jpg")] bg-cover w-full h-full xl:bg-center xl:w-[80%] min-[1800px]:w-[60%]'></div>
+      </div>
+      {/* LoginForm container for flex layout */}
+      <div className="flex items-center justify-center flex-grow xl:justify-start">
+        <LoginForm />
+      </div>  
     </div>
   );
 }
