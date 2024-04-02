ff94f91663595b5b387ffe4988a63bed3090fa38
andrewb2011 <andrew.ballard@ufl.edu>
2024-04-02 13:20:57 -0400

    changed NEW POST button to conditionally render only for donors

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,12 +1,17 @@
 import DonationList from "../features/donation/components/DonationList";
 
 function Feed({ user }) {
+  console.log(user);
   return (
     <div className="mx-64 mt-5 font-robotoslab ">
       <div className="flex flex-row">
-        <button className="h-10 text-white rounded w-60 bg-orange hover:bg-[#E37410] hover:font-bold hover:text-lg">
-          New Post
-        </button>
+        {user.user_metadata.role === "donor" ? (
+          <button className="h-10 text-white rounded w-60 bg-orange hover:bg-[#E37410] hover:font-bold hover:text-lg">
+            New Post
+          </button>
+        ) : (
+          ""
+        )}
         <label className="self-end pl-10">
           <input className="mr-2" type="checkbox" />
           Show My Posts
