d33b736dea900a4566810f76d7fd2bee2a23a6f3
William Garcia <wwoodsgarcia@ufl.edu>
2024-04-23 13:35:42 -0400

    styled footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
-    <footer className="flex-shrink-0 w-full h-24 bg-footer sm:basis-24">
-      <p className="text-white font-robotoslab ">&copy;NourishLink 2024</p>
+    <footer className="flex-shrink-0 w-full h-24 bg-footer sm:basis-24 flex items-center justify-center">
+      <p className="text-white font-robotoslab ">
+        &copy;{new Date().getFullYear()} NourishLink. All rights reserved.
+      </p>
     </footer>
   );
 }
